40d7b8ff3506632725d6e0e7f7b47b8151d53dc9
Joleen Chong <joleenchonk@gmail.com>
2023-08-03 12:31:15 +0800

    Implemented basic game function

--- rps.js
+++ rps.js
@@ -1,8 +1,7 @@
-//this function returns Rock, paper, scissors randomly
 function getComputerChoice()
 {
     const choicelist =  ["rock", "paper", "scissors"]
-    let choice = array[Math.floor(Math.random() * choicelist.length)]
+    let choice = choicelist[Math.floor(Math.random() * choicelist.length)]
     return choice;
 
 }
@@ -10,9 +9,62 @@ function getComputerChoice()
 function playRound(playerSelection, computerSelection)
 {
     let playerWin = false;
+    let winLoseText;
+
+    //rock beats scissors, scissors beats paper, paper beats rock
+    if (playerSelection == computerSelection)
+    {
+        playerWin = null;
+    }
+    else
+    {
+        if (playerSelection == "rock")
+        {
+            playerWin = computerSelection == "scissors" ? true : false
+        }
+        else if (playerSelection == "paper")
+        {
+            playerWin = computerSelection == "rock" ? true : false
+        }
+        else if (playerSelection == "scissors")
+        {
+            playerWIn = computerSelection == "paper" ? true : false
+        }
+        else
+        {
+            window.alert("Invalid input")
+        }
+    }
+    if (playerWin)
+    {
+        winLoseText = "You win, " + playerSelection + " beats " + computerSelection;
+    }
+    else if (playerWin == null)
+    {
+        winLoseText = "Draw."
+    }
+    else
+    {
+        winLoseText = "You lose, " + computerSelection + " beats " + playerSelection;
+    }
+    window.alert(winLoseText)
     
+    return playerWin;
+
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+function game()
+{
+    let playerScore = 0;
+    let computerScore = 0;
+    let checkWin, playerSelection;
+    for (let i = 0; i < 5; i++)
+    {
+        playerSelection = prompt("Enter choice: ", "rock")
+        checkWin = playRound(playerSelection.toLowerCase(), getComputerChoice())
+        checkWin ? playerScore++ : computerScore++
+    }
+    playerScore > computerScore ? window.alert("Congrats! You win") : window.alert("You lose. Game Over.")
+}
+
+game();
